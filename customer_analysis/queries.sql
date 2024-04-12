f80636561df6dc05c84afbf03d268bdf27f659b3
Jason Do <138195365+jasondo-da@users.noreply.github.com>
2024-04-11 17:52:00 -0700

    Update queries.sql

--- customer_analysis/queries.sql
+++ customer_analysis/queries.sql
@@ -1,6 +1,6 @@
 /* Creating the dataset table */
 
-CREATE TABLE shopping_behavior.sb_customer_orders
+CREATE TABLE shopping_behavior.customer_orders
 	(customer_id SMALLINT UNSIGNED,
     age TINYINT UNSIGNED,
     gender VARCHAR(20),
@@ -8,7 +8,7 @@ CREATE TABLE shopping_behavior.sb_customer_orders
     category VARCHAR(20),
     purchase_total SMALLINT UNSIGNED,
     location VARCHAR(20),
-    size ENUM('small', 'med', 'large', 'xl'),
+    size VARCHAR(20),
     color VARCHAR(20),
     season ENUM('spring', 'summer', 'fall', 'winter'),
     review_rating float(2,1),
@@ -18,24 +18,68 @@ CREATE TABLE shopping_behavior.sb_customer_orders
     promo_code_applied ENUM('yes', 'no'),
     previous_orders SMALLINT UNSIGNED,
     payment_method VARCHAR(20),
-    purchase_frequency SMALLINT UNSIGNED,
+    purchase_frequency VARCHAR(20),
     
     CONSTRAINT unique_id PRIMARY KEY (customer_id)
     )
 
+/* Filling in the new table with data from the original dataset */
+	
+INSERT INTO customer_orders (
+customer_id,
+age,
+gender,
+item_sub_cat,
+category,
+purchase_total,
+location,
+size,
+color,
+season,
+review_rating,
+subscription_status,
+shipping_type,
+discount_applied,
+promo_code_applied,
+previous_orders,
+payment_method,
+purchase_frequency
+)
+
+SELECT
+customer_id,
+age,
+gender,
+items_purchased,
+category,
+purchase_amount,
+location,
+size,
+color,
+season,
+review_rating,
+subscription_status,
+shipping_type,
+discount_applied,
+promo_code_used,
+previous_purchases,
+payment_method,
+frequency_of_purchases
+FROM sb_clean
+
 /* Discovering the gender diversity of the total clientele  */
 
-SELECT sb.gender, COUNT(sb.gender)
-FROM sb_customer_orders sb
-GROUP BY sb.gender
-ORDER BY avg(sb.age) DESC
+SELECT co.gender, COUNT(co.gender)
+FROM customer_orders co
+GROUP BY co.gender
+ORDER BY avg(co.age) DESC
 
 /* Discovering the age diversity of the total clientele  */
 
-SELECT sb.gender, avg(sb.age)
-FROM sb_customer_orders sb
-GROUP BY sb.gender
-ORDER BY avg(sb.age) DESC
+SELECT co.gender, avg(co.age)
+FROM customer_orders co
+GROUP BY co.gender
+ORDER BY avg(co.age) DESC
 
 /*  */
 
